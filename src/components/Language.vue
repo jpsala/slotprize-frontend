253222fc95c18b3d41e19e7fe29864e69af9ed6b
JP <jpsala@gmail.com>
2020-09-03 00:03:24 -0300

    now using deepClone in languages so to not work on the original data, now cancel does nothing on it

--- src/components/Language.vue
+++ src/components/Language.vue
@@ -1,10 +1,10 @@
 <template>
-  <q-card class="my-card">
+  <q-card class="my-card" v-if="languageCopy">
     <input autofocus ref='fileInput' type="file" accept="application/JSON" @change="fileChange($event)" class="hidden">
     <q-card-section class="q-mb-lg" style="text-align: center;">
       <div class="row">
         <div class="text-h6">
-          <q-input v-model="language.languageCode" label="Language Code" />
+          <q-input v-model="languageCopy.languageCode" label="Language Code" />
         </div>
       </div>
       <div class="row">
@@ -15,7 +15,7 @@
       </div>
       <input autofocus ref='imgInput' type="file" accept="image/*" @change="imgChange($event)" class="hidden">
           <img ref="img" class="cursor-pointer" @click="$refs.imgInput.click()" style="border-radius: 20%"
-               id="output_image" :src="language.textureUrl ? language.textureUrl : missingImage">
+               id="output_image" :src="languageCopy.textureUrl ? languageCopy.textureUrl : missingImage">
     </q-card-section>
     <q-card-actions class="q-mt-lg" align="right">
       <q-btn flat @click="$refs.fileInput.click()" >Select JSON file</q-btn>
@@ -29,10 +29,12 @@
 </template>
 
 <script>
-import { reactive, toRefs, watch } from '@vue/composition-api'
+import { reactive, toRefs, watch, onMounted } from '@vue/composition-api'
 import useSession from 'src/services/useSession'
 // eslint-disable-next-line no-unused-vars
 import axios from '../services/axios'
+import clone from 'rfdc'
+
 export default {
   props: {
     language: { type: Object }
@@ -40,6 +42,7 @@ export default {
   setup (props, { emit }) {
     const { loggedIn } = useSession()
     const state = reactive({
+      languageCopy: undefined,
       imgInput: undefined,
       fileInput: undefined,
       fileForUpload: props.language.localizationUrl ?? 'Select a JSON file',
@@ -60,12 +63,15 @@ export default {
       const files = {}
       if (state.fileInput.files.length > 0) files.localization = state.fileInput.files[0]
       if (state.imgInput.files.length > 0) files.texture = state.imgInput.files[0]
-      emit('close', { language: props.language, files })
+      emit('close', { language: state.languageCopy, files })
     }
+    onMounted(() => {
+      state.languageCopy = clone()(props.language)
+    })
     watch(() => loggedIn, async () => {
     }, { immediate: true })
     watch(() => props.language, () => {
-      state.languageCopy = Object.assign({}, props.language)
+      state.languageCopy = clone()(props.language)
     }, { inmediate: true })
     return { ...toRefs(state), fileChange, submit, imgChange }
   }
