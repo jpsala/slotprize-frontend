88609b211107fb0f6201cda53c7cf2251cdb4d5a
JP <jpsala@gmail.com>
2020-11-20 17:40:23 -0300

    new balance and legals pages

---
+++ src/pages/Balance.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="q-pa-md q-gutter-md justify-center">
+    <h3 class="q-ml-xl q-pl-xl">
+      Balance
+    </h3>
+
+    <div class="text-subtitle1 text-weight-regular text-uppercase">Ads</div>
+    <q-input v-model="interstitialsRatio" label="Interstitials Ratio"/>
+
+    <div class="text-subtitle1 text-weight-regular text-uppercase">Spin</div>
+    <div class="row">
+      <q-input class="col" v-model="lapseForSpinRegeneration" label="Regeneration Time in Minutes"/>
+      <q-input class="col" v-model="maxSpinsForSpinRegeneration" label="Threshold for spin regeneration"/>
+    </div>
+
+    <div class="text-subtitle1 text-weight-regular text-uppercase">Default players wallet</div>
+    <div class="row">
+      <q-input  v-model="wallet.spins" label="Spins" class="col"/>
+      <q-input  v-model="wallet.coins" label="Coins" class="col"/>
+      <q-input  v-model="wallet.tickets" label="Tickets" class="col q-mr-sm"/>
+    </div>
+
+    <div class="text-subtitle1 text-weight-regular text-uppercase">Tickets</div>
+    <div class="row">
+      <q-input class="col" v-model="ticketPrice" label="Value in coins"/>
+    </div>
+
+    <q-btn color="primary" @click="submit"> Submit </q-btn>
+
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs, watch } from '@vue/composition-api'
+import useSession from 'src/services/useSession'
+import axios from '../services/axios'
+export default {
+  setup () {
+    const { loggedIn } = useSession()
+    const state = reactive({
+      ticketPrice: undefined,
+      wallet: {
+        spins: undefined,
+        coins: undefined,
+        tickets: undefined
+      },
+      interstitialsRatio: undefined,
+      lapseForSpinRegeneration: 0,
+      maxSpinsForSpinRegeneration: 0
+    })
+    const submit = async () => {
+      await axios.post(
+        '/slot/tickets_settings_for_crud',
+        {
+          wallet: state.wallet,
+          interstitialsRatio: state.interstitialsRatio,
+          lapseForSpinRegeneration: state.lapseForSpinRegeneration * 60,
+          maxSpinsForSpinRegeneration: state.maxSpinsForSpinRegeneration,
+          ticketPrice: state.ticketPrice
+        }
+      )
+    }
+    watch(() => loggedIn, async () => {
+      const response = await axios({
+        url: '/slot/tickets_settings_for_crud',
+        method: 'get'
+      })
+      console.log('response', response.data)
+      state.ticketPrice = response.data.ticketPrice
+      state.wallet = response.data.wallet
+      state.interstitialsRatio = response.data.interstitialsRatio
+      state.lapseForSpinRegeneration = response.data.lapseForSpinRegeneration / 60
+      state.maxSpinsForSpinRegeneration = response.data.maxSpinsForSpinRegeneration
+    }, { immediate: true })
+    return { ...toRefs(state), submit }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Legals.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="q-pa-md">
+
+    <div class="q-gutter-y-md" style="max-width: 100%">
+      <h3 class="q-ml-xl q-pl-xl" style="margin-bottom: 0; padding-bottom: 0">Legals</h3>
+      <q-separator />
+      <h4 v-if="missingLocalizations">There are missing localizations</h4>
+      <q-checkbox v-model="dontEnforce" label="dont enforce agreement" />
+      <q-option-group v-model="panel" inline :options="items" />
+      <q-tab-panels v-model="panel" animated >
+        <q-tab-panel v-for="tab of items" :name="tab.value" :key="tab.value">
+          <div class="text-subtitle1 text-weight-regular text-uppercase">{{tab.label}}</div>
+          <q-separator :spaced="true"/>
+          <div v-for="rule in tab.localizations" :key="rule.languageId" class="shadow-2 rounded-borders q-mt-md">
+              <div class="bg-grey-8 q-pa-sm text-white text-subtitle1 text-weight-regular text-uppercase">
+                {{rule.languageCode}}
+              </div>
+              <q-separator />
+              <div class="q-pa-md">
+                <q-input class="localization-input" type="textarea" v-model="rule.text" autogrow style="font-size: 120%"/>
+              </div>
+          </div>
+        </q-tab-panel>
+      </q-tab-panels>
+      <div class="row q-mt-md float-right">
+        <q-btn color="primary" @click="submit">
+          Submit
+        </q-btn>
+      </div>
+  </div>
+    </div>
+</template>
+<script>
+import axios from '../services/axios'
+import useGlobal from '../services/useGlobal'
+import useSession from '../services/useSession'
+import { reactive, toRefs, watch, onMounted, nextTick, computed } from '@vue/composition-api'
+import { alerta } from 'src/helpers'
+export default {
+  setup () {
+    console.clear()
+    const { showSpinner, hideSpinner } = useGlobal()
+    const { loggedIn } = useSession()
+    // const { isDevEnv } = useGlobal()
+    let downloadingData = false
+    const state = reactive({
+      panel: '',
+      items: [],
+      dontEnforce: false,
+      missingLocalizations: computed(() => {
+        for (const item of state.items) {
+          for (const localization of item.localizations) {
+            if (localization.text === '') return true
+          }
+        }
+        return false
+      })
+    })
+    const submit = async () => {
+      showSpinner()
+      const resp = await axios.post('/slot/legals_for_crud', { items: state.items, dontEnforce: state.dontEnforce })
+      hideSpinner()
+      if (Number(resp.data) > 0) await alerta('Saved', `${resp.data} Profiles where updated`)
+      console.log('resp', resp)
+    }
+    const getData = async () => {
+      if (state.items.length > 0 || downloadingData) return
+      try {
+        downloadingData = true
+        showSpinner()
+        const resp = await axios.get('/slot/legals_for_crud')
+        state.items = resp.data
+        state.panel = state.items[0].value
+        nextTick(() => { document.querySelector('.localization-input').focus() })
+      } finally {
+        downloadingData = false
+        hideSpinner()
+      }
+    }
+    onMounted(getData)
+    watch(() => loggedIn.value, async (v) => {
+      console.log('watch')
+      await getData()
+    }, { inmediate: true })
+    return { ...toRefs(state), submit }
+  }
+}
+</script>
