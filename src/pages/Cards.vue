bd5582c1b843d868105b25197dacbe6532eabad5
JP <jpsala@gmail.com>
2020-12-04 00:28:19 -0300

    in cards chest rewards can be modified

--- src/pages/Cards.vue
+++ src/pages/Cards.vue
@@ -2,6 +2,60 @@
   <div class="q-pa-md q-gutter-md justify-center">
     <div class="text-h3 q-ml-xl">Cards</div>
     <q-separator/>
+    <div  v-if="chestPremium">
+      <div class="text-h5 q-ml-md">Chest configuration</div>
+      <div class="text-h7 q-ml-md">Premium <q-btn fab-mini color="primary" icon="save" @click="saveChest('chestPremiumRewards')" class="q-ml-sm" /></div>
+
+      <div class="row" style="width: 400px">
+        <q-input :disable="newReward !== undefined" class="col" v-model="chestPremium.priceAmount" type="text" label="Price Amount" />
+        <q-select :disable="newReward !== undefined" class="col" label="Reward Type" v-model="chestPremium.priceCurrency" :options="paymentOptions" stack-label/>
+      </div>
+      <div class="row">
+        <div class="text-h8 q-ml-lg" style="width: 60px; align-self: center">Rewards</div>
+        <q-btn :disable="newReward !== undefined" fab-mini color="primary" icon="add" @click="addPremiumReward(chestPremium)" class="q-ml-sm" />
+      </div>
+      <div v-for="(reward, index) of chestPremium.rewards" :key="index" >
+        <q-separator class="q-ml-lg" style="width:375px"/>
+        <div class="row" style="width: 400px">
+            <q-input :disable="newReward !== undefined && !reward.isNew" class="col q-ml-lg" v-model="reward.amount" type="text" label="Price Amount" />
+            <q-select :disable="newReward !== undefined && !reward.isNew" class="col" label="Reward Type" v-model="reward.type" :options="paymentOptions" stack-label/>
+            <q-btn v-show="reward.isNew" fab-mini color="red-6" icon="close" @click="cancelPremiumReward(chestPremium)" class="q-ml-sm" />
+            <q-btn v-show="reward.isNew" fab-mini color="primary" icon="check" @click="savePremiumReward(index)" class="q-ml-sm" />
+        </div>
+      </div>
+    </div>
+    <div  v-if="chestRegular" class="q-mt-lg">
+      <q-separator spaced="15px" />
+      <div class="text-h7 q-ml-md">Regular<q-btn fab-mini color="primary" icon="save" @click="saveChest('chestRegularRewards')" class="q-ml-sm" /></div>
+      <div class="row" style="width: 400px">
+        <q-input :disable="newReward !== undefined" class="col" v-model="chestRegular.priceAmount" type="text" label="Price Amount" />
+        <q-select :disable="newReward !== undefined" class="col" label="Reward Type" v-model="chestRegular.priceCurrency" :options="paymentOptions" stack-label/>
+      </div>
+      <div class="row">
+        <div class="text-h8 q-ml-lg" style="width: 60px; align-self: center">Rewards</div>
+        <q-btn :disable="newReward !== undefined" fab-mini color="primary" icon="add" @click="addRegularReward(chestRegular)" class="q-ml-sm" />
+
+      </div>
+      <div v-for="(reward, index) of chestRegular.rewards" :key="index" >
+        <q-separator class="q-ml-lg" style="width:375px"/>
+        <div class="row" style="width: 400px">
+            <q-input :disable="newReward !== undefined && !reward.isNew" class="col q-ml-lg" v-model="reward.amount" type="text" label="Price Amount" />
+            <q-select :disable="newReward !== undefined && !reward.isNew" class="col" label="Reward Type" v-model="reward.type" :options="paymentOptions" stack-label/>
+            <q-btn v-show="reward.isNew" fab-mini color="red-6" icon="close" @click="cancelRegularReward(chestRegular)" class="q-ml-sm" />
+            <q-btn v-show="reward.isNew" fab-mini color="primary" icon="check" @click="saveRegularReward(index)" class="q-ml-sm" />
+        </div>
+      </div>
+      <q-separator spaced="50px" />
+    </div>
+<!--
+  chestPremium:
+  priceAmount: 1
+  priceCurrency: "spin"
+  rewards: Array(1)
+  0:
+  amount: 1
+  type: "spin"
+-->
     <div class="text-h5 q-ml-md">Drop Rate Table</div>
     <card-drop-rate-table />
     <q-separator spaced="30px"/>
@@ -153,7 +207,15 @@ export default {
       emptyCard: undefined,
       cardSetBackup: undefined,
       cardBackup: undefined,
-      cardEditing: undefined
+      cardEditing: undefined,
+      chestRegular: undefined,
+      chestPremium: undefined,
+      newReward: undefined,
+      chestPremiumBackup: undefined,
+      chestRegularBackup: undefined,
+      newRewardBackup: undefined,
+      chestPremiumDirty: false,
+      chestRegularDirty: false
     })
     const addCardSet = () => {
       state.newCardSet = clone()(state.emptyCardSet)
@@ -191,6 +253,17 @@ export default {
       state.newCard = undefined
       return response
     }
+    const saveChest = async (chestName) => {
+      const chest = chestName === 'chestPremiumRewards' ? state.chestPremium : state.chestRegular
+      console.log(chestName, chest)
+      const response = await axios({
+        method: 'post',
+        url: 'slot/cards_for_crud_chest',
+        data: { name: chestName, chest }
+      })
+      state.chestPremiumDirty = false
+      return response
+    }
     const cancelSetEdition = (cardSet) => {
       const idx = state.cardSets.findIndex(_cardSet => _cardSet.id === cardSet.id)
       if (cardSet.id !== -1) state.cardSets[idx] = clone()(state.cardSetBackup)
@@ -215,7 +288,7 @@ export default {
       }
     }
     const deleteCardSet = async (cardSet) => {
-      if (!await confirma('Are you sure you want to delete this card set with all of it\'s cards')) { return }
+      if (!(await confirma('Are you sure you want to delete this card set with all of it\'s cards'))) { return }
       showSpinner()
       try {
         const resp = await axios.delete('/slot/card_set_for_crud?cardSetId=' + cardSet.id)
@@ -262,6 +335,39 @@ export default {
         await alerta('Error saving data', err)
       }
     }
+    const addPremiumReward = async () => {
+      state.newReward = { amount: 1, type: 'coin', isNew: true }
+      state.chestPremium.rewards.push(state.newReward)
+    }
+    const cancelPremiumReward = (rewardIdx) => {
+      const idx = state.chestPremium.rewards.findIndex(reward => reward.isNew)
+      state.chestPremium.rewards.splice(idx, 1)
+      state.newReward = undefined
+    }
+    const savePremiumReward = async (rewardIdx) => {
+      state.chestPremium.rewards[rewardIdx].isNew = false
+      state.newReward = undefined
+    }
+    const addRegularReward = async () => {
+      state.newReward = { amount: 1, type: 'coin', isNew: true }
+      state.chestRegular.rewards.push(state.newReward)
+    }
+    const cancelRegularReward = (rewardIdx) => {
+      const idx = state.chestRegular.rewards.findIndex(reward => reward.isNew)
+      state.chestRegular.rewards.splice(idx, 1)
+      state.newReward = undefined
+    }
+    const saveRegularReward = async (rewardIdx) => {
+      state.chestRegular.rewards[rewardIdx].isNew = false
+      state.newReward = undefined
+    }
+    watch(() => state.chestPremium, (a, b) => {
+      if (!a || !b) return
+      console.log('watch', a, b)
+      console.log('state.chestPremiumBackup === state.chestPremium', state.chestPremiumBackup === state.chestPremium)
+      console.log('state.chestPremiumBackup === state.chestPremium', state.chestPremiumBackup, state.chestPremium)
+      state.chestPremiumDirty = (state.chestPremiumBackup !== state.chestPremium)
+    }, { deep: true })
     // watch and get data from backend
     watch(() => loggedIn, async () => {
       const response = await axios({
@@ -277,8 +383,30 @@ export default {
       state.languages = response.data.languages
       state.emptyCard = clone()(response.data.newCard)
       state.emptyCardSet = clone()(response.data.newCardSet)
+      state.chestRegular = response.data.chestRegular
+      state.chestRegularBackup = clone()(response.data.chestRegular)
+      state.chestPremium = response.data.chestPremium
+      state.chestPremiumBackup = clone()(response.data.chestPremium)
     }, { immediate: true })
-    return { ...toRefs(state), addCardSet, submit, editSet, cancelSetEdition, saveCard, addCard, cancelCardAdd, deleteCard, deleteCardSet }
+    return {
+      ...toRefs(state),
+      addCardSet,
+      submit,
+      editSet,
+      cancelSetEdition,
+      saveCard,
+      addCard,
+      cancelCardAdd,
+      deleteCard,
+      deleteCardSet,
+      addPremiumReward,
+      savePremiumReward,
+      cancelPremiumReward,
+      addRegularReward,
+      saveRegularReward,
+      cancelRegularReward,
+      saveChest
+    }
   }
 }
 </script>
