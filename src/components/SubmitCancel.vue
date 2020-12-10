1df644e5b0fcdc21338ad73830743c6a44aa5f7b
JP <jpsala@gmail.com>
2020-12-10 19:49:14 -0300

    added saveColor, cancelColor and sie to sumbit-cancel component

--- src/components/SubmitCancel.vue
+++ src/components/SubmitCancel.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="self-center">
   <q-btn-group push outline :class="right ? 'float-right' : '' ">
-    <q-btn push :disable="cancelDisable || disable" size="11px"  color="red-7" :label="showLabels ? labelCancel : ''" icon="close" @click="$emit('cancel')"  />
-    <q-btn push :disable="submitDisable || disable" size="11px"  color="green-7" :label="showLabels ? labelSave : ''" icon="save" @click="$emit('submit')" class="" />
+    <q-btn push :disable="cancelDisable || disable" :size="size"  :color="cancelColor" :label="showLabels ? labelCancel : ''" icon="close" @click="$emit('cancel')"  />
+    <q-btn push :disable="submitDisable || disable" :size="size"  :color="saveColor" :label="showLabels ? labelSave : ''" icon="save" @click="$emit('submit')" class="" />
   </q-btn-group>
   <span  style="clear: both">&nbsp;</span>
 </div>
@@ -17,7 +17,10 @@ export default {
     right: { default: false },
     showLabels: { default: true },
     labelSave: { default: 'Save' },
-    labelCancel: { default: 'Cancel' }
+    labelCancel: { default: 'Cancel' },
+    saveColor: { default: 'green-7' },
+    cancelColor: { default: 'red-7' },
+    size: { default: 'md' }
   },
   setup (props) {
     const state = reactive({
