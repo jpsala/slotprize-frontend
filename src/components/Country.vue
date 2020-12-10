02d0bf9f09b11f05bb88c6be10c9375354550030
JP <jpsala@gmail.com>
2020-12-10 14:56:04 -0300

    small typo

--- src/components/Country.vue
+++ src/components/Country.vue
@@ -106,7 +106,7 @@ export default {
       state.reader.readAsDataURL(event.target.files[0])
     }
     const submit = () => {
-      var files = state.imgInput.files
+      const files = state.imgInput.files
       emit('close', { country: state.countryCopy, files })
     }
     onMounted(() => {
